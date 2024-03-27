38d44290a9fc91da87c382f1f789bdc38f8f83ce
DimitarID <34337664+DimitarID@users.noreply.github.com>
2024-03-27 21:22:56 +0200

    initial comit

---
+++ 03.2 PB-CSharp-Nested-Conditional-Statements-Lab/03. Animal Type/Program.cs
@@ -0,0 +1,16 @@
+﻿string animal = Console.ReadLine();
+
+if (animal == "dog")
+{
+    Console.WriteLine("mammal");
+}
+
+else if (animal == "crocodile" || animal == "tortoise" || animal == "snake")
+{
+    Console.WriteLine("reptile");
+}
+
+else
+{
+    Console.WriteLine("unknown");
+}
