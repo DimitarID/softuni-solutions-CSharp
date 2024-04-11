42a190733bd1ea6570ced823c98b8632d588bcc1
DimitarID <34337664+DimitarID@users.noreply.github.com>
2024-04-11 22:25:43 +0300

    initial commit

---
+++ 04.2 PB-CSharp-For-Loop-Lab/02. Numbers N-1/Program.cs
@@ -0,0 +1,6 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+for (int i = N; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
---
+++ 04.2 PB-CSharp-For-Loop-Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+int oddSum = 0;
+int evenSum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    int element = int.Parse(Console.ReadLine());
+
+    if (i % 2 == 0)
+    {
+        evenSum += element;
+    }
+    else
+    {
+        oddSum += element;
+    }
+}
+
+if (evenSum == oddSum)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {evenSum}");
+}
+else
+{
+    int finsum = Math.Abs(oddSum - evenSum);
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {finsum}");
+}
