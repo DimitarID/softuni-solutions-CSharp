cd18eba688cbc75523d0d59ee659e8409ba6078d
DimitarID <34337664+DimitarID@users.noreply.github.com>
2024-04-15 21:33:18 +0300

    initial commit

---
+++ 04.3 PB-CSharp-For-Loop-Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int firstNumber = int.Parse(Console.ReadLine());
+int max = firstNumber;
+int min = firstNumber;
+int sum = firstNumber;
+
+for (int i = 1; i < n; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    sum += num;
+    if (num > max)
+    {
+        max = num;
+    }
+    if (num < min)
+    {
+        min = num;
+    }
+}
+
+if (sum - max == max)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine("Sum = " + max);
+}
+else
+{
+    int diff = Math.Abs(max - (sum - max));
+    Console.WriteLine("No");
+    Console.WriteLine("Diff = " + diff);
+}
---
+++ 04.3 PB-CSharp-For-Loop-Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,36 @@
+﻿{
+    int age = int.Parse(Console.ReadLine());
+    double priceOfWashingMachine = double.Parse(Console.ReadLine());
+    double priceOfToys = double.Parse(Console.ReadLine());
+
+    int moneyStart = 10;
+    int moneySaved = 0;
+    int toys = 0;
+    int brother = 0;
+    double allMoney = 0.0d;
+
+    for (int i = 1; i <= age; i++)
+    {
+        if (i % 2 == 0 && i != 1)
+        {
+            moneySaved = moneySaved + moneyStart;
+            moneyStart += 10;
+            brother++;
+        }
+        else if (i % 2 != 0 || i == 1)
+        {
+            toys++;
+        }
+    }
+    allMoney = toys * priceOfToys + moneySaved - brother;
+    if (allMoney >= priceOfWashingMachine)
+    {
+        allMoney = allMoney - priceOfWashingMachine;
+        Console.WriteLine($"Yes! {allMoney:f2}");
+    }
+    else
+    {
+        allMoney = Math.Abs(allMoney - priceOfWashingMachine);
+        Console.WriteLine($"No! {allMoney:f2}");
+    }
+}
