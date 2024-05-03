eb807e4b7581543f4d3dcee10e79454ee4df79a2
DimitarID <34337664+DimitarID@users.noreply.github.com>
2024-05-03 09:28:37 +0300

    initial commit

---
+++ 06.2 PB-CSharp-Nested-Loops-Lab/05. Travelling/Program.cs
@@ -0,0 +1,20 @@
+﻿while (true)
+{
+    string destination = Console.ReadLine();
+
+    if (destination == "End")
+    {
+        break;
+    }
+
+    double sum = 0;
+    double budget = double.Parse(Console.ReadLine());
+
+    while (budget > sum)
+    {
+        double sumInput = double.Parse(Console.ReadLine());
+        sum += sumInput;
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+}
